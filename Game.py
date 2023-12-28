f48d585030a548181b9fe7004a949e97d0191f8f
tiijaay1 <117973739+tiijaay1@users.noreply.github.com>
2023-12-28 14:17:08 +0100

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,23 +1,16 @@
-# Create a new directory for your game
+
 mkdir simple-game
 
-# Change into the directory
-cd simple-game
-
-# Initialize a Git repository
+cd simple-game 
 git init
 
-# Create a README file
 echo "# Simple Game" >> README.md
 
-# Create a main game file (e.g., game.py)
 touch game.py
 
-# Add and commit the changes
 git add .
 git commit -m "Initial commit"
 
-# Create a new repository on GitHub (replace 'your-username' with your GitHub username)
 git remote add origin https://github.com/tiijaay1/simple-game.git
 git branch -M main
 git push -u origin main
